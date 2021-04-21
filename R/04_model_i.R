0b66e278e2d95fcdcdb0f297b6e0863e2fa5748c
tortou <s163688@student.dtu.dk>
2021-04-21 09:58:21 +0000

    Creating first PCA plot: Scatter plot in PC1 and PC2.

--- R/04_model_i.R
+++ R/04_model_i.R
@@ -23,7 +23,12 @@ pca_fit <- my_data_clean_aug %>%
   prcomp(scale = TRUE) # do PCA on scaled data
 
 # Visualise data ----------------------------------------------------------
-
+pca_fit %>%
+  augment(my_data_clean_aug) %>% # add original dataset back in
+  ggplot(aes(.fittedPC1, .fittedPC2, color = site)) + 
+  geom_point(size = 1.5) +
+  labs(x = "PC 1", y = "PC 2", title = "Scatter plot in Top 2 Principal Components") +
+  theme_half_open(12) + background_grid()
 
 # Write data --------------------------------------------------------------
 
