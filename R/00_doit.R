a94ae47793e1cd53d27996c3f104bca368c5b80f
Martin-e-k <martin_e_k@hotmail.com>
2021-04-21 09:09:01 +0000

    Nu burde doit kunne køre det hele

--- R/00_doit.R
+++ R/00_doit.R
@@ -4,3 +4,9 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 
+source(file = "R/01_load.R")
+source(file = "R/02_clean.R")
+source(file = "R/03_augment.R")
+source(file = "R/04_model_i.R")
+source(file = "R/05_model_ii.R")
+
