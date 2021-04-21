e061ca19ae9d102850fad375b180ff1d2f0b984f
Martin-e-k <martin_e_k@hotmail.com>
2021-04-21 09:00:54 +0000

    source functions

--- R/04_model_i.R
+++ R/04_model_i.R
@@ -6,7 +6,7 @@ rm(list = ls())
 library("tidyverse")
 
 # Define functions --------------------------------------------------------
-#source(file = "R/99_project_functions.R")
+source(file = "R/99_project_functions.R")
 
 # Load data ---------------------------------------------------------------
 my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv.gz")
