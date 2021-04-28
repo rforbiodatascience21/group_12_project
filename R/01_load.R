0cf5ffe673533b7e994ec5edaa89afb663db1712
nrt94 <nrthornval@hotmail.com>
2021-04-28 07:46:35 +0000

    jk

--- R/01_load.R
+++ R/01_load.R
@@ -1,12 +1,11 @@
 # Clear workspace ---------------------------------------------------------
 rm(list = ls())
 
-
 # Load libraries ----------------------------------------------------------
-<<<<<<< HEAD
-library("tidyverse")
-library("biom")
-=======
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+# install.packages("BiocManager")
+#BiocManager::install(version = "3.12")
+#BiocManager::install("phyloseq")
 library(tidyverse)
 library(phyloseq)
 library(readxl)
@@ -18,13 +17,8 @@ source(file = "R/99_functions.R")
 
 
 # Load raw data and save it------------------------------------------------
-<<<<<<< HEAD
-SPE = read_biom(file = "data/raw/SPE_pitlatrine.csv")
-
-=======
 data = import_biom("data/raw/feature-table_taxonomy.biom")
 metadata = read_xlsx("data/raw/GE_mapfile.xlsx")
->>>>>>> 59d28168825b287a79a0e10c9d3bce7bc8f7de55
 
 
 # Write data --------------------------------------------------------------
