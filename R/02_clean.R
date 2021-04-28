81b3b10e7dc6b07b5583602646a95845d78d144c
nrt94 <nrthornval@hotmail.com>
2021-04-28 08:34:13 +0000

    new version

--- R/02_clean.R
+++ R/02_clean.R
@@ -11,16 +11,18 @@ source(file = "R/99_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-SPE <- read_tsv(file = "data/SPE_pitlatrine.tsv.gz")
-ENV <- read_tsv(file = "data/ENV_pitlatrine.tsv.gz")
+data <- read_tsv(file = "data/01_data.tsv.gz")
+metadata <- read_tsv(file = "data/01_meta_data.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean = SPE %>%
-  pivot_longer(cols = -Taxa,
-               names_to = "Samples",
-               values_to = "OTU_Count") %>%
-  full_join(ENV, by = "Samples")
-
+my_data_clean = data %>%
+  #merging data
+  full_join(metadata, by = c("Sample"="#SampleID")) %>%
+  #only select samples with metadata
+  filter(., Description != "N") %>%
+  select(., -Rank8, -Rank9, -Rank10, -Rank11, -Rank12, -Rank13, -Rank14, 
+         -Rank15, -BarcodeSequence, -LinkerPrimerSequence, -ReversePrimer)
+         
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data_clean,
           file = "data/02_my_data_clean.tsv.gz")
