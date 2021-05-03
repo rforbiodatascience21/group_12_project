da3c89538f6cfa7198ec8e566c8b328eb11aee6d
nrt94 <nrthornval@hotmail.com>
2021-05-03 08:37:57 +0000

    changes to functions and plot

--- R/99_functions.R
+++ R/99_functions.R
@@ -1,16 +1,5 @@
 # Define project functions ------------------------------------------------
-library("tidyverse")
-library("hues")
-
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv.gz")
-my_data_clean_small <- my_data_clean_aug %>%
-  select(Sample, Abundance, Season, Location, Phylum)
-
-
-#Function to calculate X most abundant phylum in samples while the rest is 
-#grouped in "Other"
-rel_topX <- function(data, X){
-
+topX <- function(data, X){
   total_count <- data %>% 
     group_by(Sample) %>% 
     summarise(total_abundance = sum(Abundance))
@@ -23,6 +12,13 @@ rel_topX <- function(data, X){
     top_n(., X, sum_rel_abundance) %>%
     pull(., Phylum)
   
+  return(top_phylum)
+}
+
+
+#Function to calculate X most abundant phylum in samples while the rest is 
+#grouped in "Other"
+produce_topX <- function(data, top_phylum) { 
   topX <- data %>%
     #classify all other phylum as "Other"
     mutate(Phylum = case_when(Phylum %in% top_phylum ~ Phylum, T ~ "Other")) %>% 
@@ -36,17 +32,6 @@ rel_topX <- function(data, X){
 }
 
 
-#Function to calculate vector of X most abundant phylum in samples with rest
-#is grouped in "Other"
-topX <- function(data, X){
-  top_phylum <- data %>%
-    group_by(Phylum) %>%
-    summarise(sum_abundance = sum(Abundance)) %>%
-    top_n(., X, sum_abundance) %>%
-    pull(., Phylum)
-  
-  return(top_phylum)
-}
 
 
 
