8cd6f45477b6ebbc75d0e4ced077ae3b6c7b7635
Martin-e-k <martin_e_k@hotmail.com>
2021-05-03 08:26:04 +0000

    123

---
+++ R/08_model_v.R
@@ -0,0 +1,21 @@
+# Clear workspace ---------------------------------------------------------
+rm(list = ls())
+#
+#install.packages("cowplot")
+# Load libraries ----------------------------------------------------------
+library("tidyverse")
+library("tidyr")
+library("vegan")
+
+
+# Define functions --------------------------------------------------------
+source(file = "R/99_functions.R")
+
+# Load data ---------------------------------------------------------------
+my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv.gz")
+
+# Wrangle data ------------------------------------------------------------
+nmds = metaMDS(my_data_clean_aug, distance = "bray")
+
+# Write data --------------------------------------------------------------
+
