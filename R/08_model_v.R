34856bf2b7d7413f45b5e5f87dcf5c6d9b63e297
nrt94 <nrthornval@hotmail.com>
2021-05-03 08:52:52 +0000

    model 8

--- R/08_model_v.R
+++ R/08_model_v.R
@@ -1,6 +1,6 @@
 # Clear workspace ---------------------------------------------------------
 rm(list = ls())
-#
+
 #install.packages("cowplot")
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
@@ -16,12 +16,12 @@ my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
 
-my_data_clean_aug <- my_data_clean_aug %>%
-  pivot_wider(names_from = OTU, values_from = Abundance)
+my_data_clean_aug_wide <- my_data_clean_aug %>%
+  select(., OTU, Abundance, Sample) %>%
+  pivot_wider(names_from = OTU, values_from = Abundance) %>%
+  select(., -Sample)
 
-
-
-nmds = metaMDS(my_data_clean_aug, distance = "bray")
+nmds = metaMDS(my_data_clean_aug_wide, distance = "bray")
 
 # Write data --------------------------------------------------------------
 
