6ed5f43ac5839cb85886992d47b5254c8d855f27
tortou <s163688@student.dtu.dk>
2021-05-03 08:35:26 +0000

    trying to pivot wider

--- R/08_model_v.R
+++ R/08_model_v.R
@@ -15,6 +15,12 @@ source(file = "R/99_functions.R")
 my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
+
+my_data_clean_aug <- my_data_clean_aug %>%
+  pivot_wider(names_from = OTU, values_from = Abundance)
+
+
+
 nmds = metaMDS(my_data_clean_aug, distance = "bray")
 
 # Write data --------------------------------------------------------------
