13a74f9fc192ff48ccb65bb64af9c0500b3db9b1
nrt94 <nrthornval@hotmail.com>
2021-05-07 08:08:46 +0000

    plots

--- R/99_functions.R
+++ R/99_functions.R
@@ -1,8 +1,14 @@
 # Define project functions ------------------------------------------------
 topX <- function(data, X){
-  top_phylum <- data %>%
+  total_abun <- data %>% 
+    group_by(Sample) %>% 
+    summarise(total_abundance = sum(Abundance))
+  
+  top_phylum <- data %>% 
+    left_join(total_abun) %>% 
+    mutate(rel_abundance = Abundance / total_abundance) %>% 
     group_by(Phylum) %>%
-    summarise(sum_rel_abundance) %>% 
+    summarise(sum_rel_abundance = sum(rel_abundance)) %>% 
     top_n(X, sum_rel_abundance) %>%
     pull(Phylum)
   
@@ -14,7 +20,7 @@ topX <- function(data, X){
 #grouped in "Other"
 produce_topX <- function(data, top_phylum) { 
   topX <- data %>%
-    mutate(Phylum = case_when(Phylum %in% top_phylum ~ Phylum, T ~ "Other")) %>% 
+    mutate(Phylum = case_when(Phylum %in% topX_phylum ~ Phylum, T ~ "Other")) %>% 
     group_by(Sample, Phylum, Location, Season) %>% 
     summarise(Phylum_abundance = sum(Abundance)) %>%
     ungroup()
