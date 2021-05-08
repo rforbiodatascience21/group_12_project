a4a5bffad39b13835d6b65e8f140841e91344fed
nrt94 <nrthornval@hotmail.com>
2021-05-08 13:38:50 +0000

    new super awesome table

--- R/09_model_table.R
+++ R/09_model_table.R
@@ -20,7 +20,8 @@ data <- read_tsv(file = "data/01_data.tsv.gz")
 # Wrangle data ------------------------------------------------------------
 #merge datasets but without any filtering
 my_data_merged = data %>%
-  full_join(metadata, by = c("Sample"="#SampleID"))
+  full_join(metadata, 
+            by = c("Sample"="#SampleID"))
 
 n_samples_data <- my_data_merged %>%
   select(Sample, OTU) %>%
@@ -28,44 +29,46 @@ n_samples_data <- my_data_merged %>%
             OTU = n_distinct(OTU)) %>% 
   pivot_longer(cols = c(Samples, OTU), 
                names_to = " ", 
-               values_to = "Total number of")
+               values_to = "Total")
 
-n_samples_season <- my_data_merged %>% 
+n_samples_season <- my_data_merged %>%
+  filter(Abundance != 0) %>% 
   group_by(Season) %>%
   summarise(Samples = n_distinct(Sample),
             OTU = n_distinct(OTU)) %>% 
   pivot_longer(cols = - Season,
-               names_to = "Seasons") %>% 
-  pivot_wider(names_from = Seasons,
-              values_from = value)
+               names_to = " ") %>% 
+  pivot_wider(names_from = Season,
+              values_from = value) %>% 
+  rename("No season" = None)
     
 n_samples_location <- my_data_merged %>%
+  filter(Abundance != 0) %>% 
   group_by(Location) %>%
-  summarise(Samples = n(Sample)
-            ) %>% 
-  pivot_longer(cols = -Location,
-             names_to = "Locations") %>% 
-  pivot_wider(names_from = Locations,
-              values_from = value)
-  relocate(Other = N, Upstream, Wastewater, Discharge, Downstream)
+  summarise(Samples = n_distinct(Sample),
+            OTU = n_distinct(OTU)) %>% 
+  pivot_longer(cols = - Location,
+             names_to = " ") %>% 
+  pivot_wider(names_from = Location,
+              values_from = value) %>%  
+  relocate(" ", "No location" = N, Upstream, Wastewater, Discharge, Downstream)
 
 
 #combine info to one table
-table_info <-  
+table_info <- n_samples_data %>% 
+  left_join(n_samples_location) %>% 
+  left_join(n_samples_season)
 
 table <- table_info %>%
-  tab_spanner(label = " ",
-              columns = "Total number") %>% 
+  gt() %>% 
   tab_spanner(label = "Season",
-              columns = vars(None, Summer, Winter)) %>% 
+              columns = vars("No season", Summer, Winter)) %>% 
   tab_spanner(label = "Location",
-              columns = vars())
-
-    
-
-
-
+              columns = vars("No location", Upstream, Wastewater, 
+                             Discharge, Downstream))
 
 
 # Write data --------------------------------------------------------------
-
+gtsave(table,
+       filename = "table1.html",
+       path = "/cloud/project/figures")
