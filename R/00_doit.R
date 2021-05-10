586f9a3a501088849afa1f07da636773b28634e9
Martin-e-k <martin_e_k@hotmail.com>
2021-05-10 13:44:09 +0000

    removed useless whitespaces

--- R/00_doit.R
+++ R/00_doit.R
@@ -4,8 +4,6 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 
-
-
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
