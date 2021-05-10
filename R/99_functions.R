423aab1ee01dc2681f75e7dab377d14d7ef24457
nrt94 <nrthornval@hotmail.com>
2021-05-10 13:34:44 +0000

    functions

--- R/99_functions.R
+++ R/99_functions.R
@@ -1,6 +1,7 @@
 # Define project functions ------------------------------------------------
 
 
+# Define global variables -------------------------------------------------
 #vector of order of Location
 location_order = c("Upstream", "Wastewater", "Discharge", "Downstream" )
 
